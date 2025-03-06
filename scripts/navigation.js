87a2f924a8daac303f2ebdb1199fa55eed521c1e
Alitares27 <aldair2795@hotmail.com>
2025-03-06 18:00:44 -0300

    Refactor scripts: replace script.js with dates.js and navigation.js for improved organization and functionality

--- scripts/navigation.js
+++ scripts/navigation.js
@@ -1,9 +1,3 @@
-const currentYear = new Date().getFullYear();
-document.getElementById("year").innerHTML = currentYear;
-
-const lastModified = document.lastModified;
-document.getElementById("modification").innerHTML = lastModified;
-
 // toggle menu in small view
 const mainNav = document.querySelector('.navigation');
 const navbutton = document.querySelector('#menu');
