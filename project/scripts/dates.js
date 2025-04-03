03aa7003f3e84555c1f58a8aa9c3a2a43a8867d2
Alitares27 <aldair2795@hotmail.com>
2025-04-03 17:44:24 -0300

    Add initial HTML structure, navigation scripts, and dynamic footer updates

---
+++ project/scripts/dates.js
@@ -0,0 +1,7 @@
+const currentYear = new Date().getFullYear();
+document.getElementById("year").innerHTML = currentYear;
+
+const lastModified = document.lastModified;
+document.getElementById("modification").innerHTML = lastModified;
+
+
---
+++ project/scripts/navigation.js
@@ -0,0 +1,8 @@
+// toggle menu in small view
+const mainNav = document.querySelector('.navigation');
+const navbutton = document.querySelector('#menu');
+
+navbutton.addEventListener('click', () => {
+  mainNav.classList.toggle('show');
+  navbutton.classList.toggle('show');
+});
