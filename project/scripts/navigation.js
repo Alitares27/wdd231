b20082698416634b6903a34a4312f1c4923104f0
Alitares27 <aldair2795@hotmail.com>
2025-04-12 09:52:03 -0300

    feat: Implement active link styling and enhance hover effects in navigation

--- project/scripts/navigation.js
+++ project/scripts/navigation.js
@@ -6,3 +6,15 @@ navbutton.addEventListener('click', () => {
   mainNav.classList.toggle('show');
   navbutton.classList.toggle('show');
 });
+
+const currentUrl = window.location.pathname.split("/").pop();
+  
+  const navLinks = document.querySelectorAll(".navigation a");
+
+  navLinks.forEach(link => {
+    const linkHref = link.getAttribute("href");
+
+    if (linkHref === currentUrl || (linkHref === "index.html" && currentUrl === "")) {
+      link.classList.add("active");
+    }
+  });
